d385fbadc00192baf3ac0fdbd8e5070bffe75cb2
sonich <s.berdnikova@onpeak.ru>
2024-08-20 17:28:01 +0300

    Чистка апи

--- src/api.js
+++ src/api.js
@@ -33,26 +33,6 @@ export const loadCoinList = async () => {
   return Object.values(data.Data);
 };
 
-// export const loadTickers = () => {
-//   if (tickersHandlers.size === 0) {
-//     return;
-//   }
-//   fetch(
-//     `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[
-//       ...tickersHandlers.keys()
-//     ].join(",")}&tsyms=USD&api-key=${API_KEY}`
-//   )
-//     .then((f) => f.json())
-//     .then((rawData) => {
-//       const updatedPrices = Object.fromEntries(
-//         Object.entries(rawData).map(([key, value]) => [key, value.USD])
-//       );
-//       Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-//         const handlers = tickersHandlers.get(currency) || [];
-//         handlers.forEach((fn) => fn(newPrice));
-//       });
-//     });
-// };
 function sendToWs(message) {
   const stringifyMessage = JSON.stringify(message);
 
@@ -95,7 +75,5 @@ export const unsubscribeFromTicker = (ticker) => {
   tickersHandlers.delete(ticker);
   unSubscribeFromTickerOnWs(ticker);
 };
-
-// setInterval(loadTickers, 5000);
 
 window.tickersHandlers = tickersHandlers;
